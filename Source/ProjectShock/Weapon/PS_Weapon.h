89cbb1b5880a691249aaa9296aeff43d4b3bf804
StrahdVonZarovich <fatih.canbekli@outlook.com>
2021-07-22 22:44:29 +0300

    Add glock animations

--- Source/ProjectShock/Weapon/PS_Weapon.h
+++ Source/ProjectShock/Weapon/PS_Weapon.h
@@ -19,7 +19,7 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	UPROPERTY(VisibleAnywhere, Category = SkeletalMesh)
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = SkeletalMesh)
 		class USkeletalMeshComponent* WeaponMesh;
 
 
---
+++ Source/ProjectShock/Weapon/PS_WeaponAnimation.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "PS_WeaponAnimation.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTSHOCK_API UPS_WeaponAnimation : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
