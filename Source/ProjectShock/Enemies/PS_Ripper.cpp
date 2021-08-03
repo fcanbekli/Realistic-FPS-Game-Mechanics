f5d53c5958514bf391459326877d547f7a5b754f
StrahdVonZarovich <fatih.canbekli@outlook.com>
2021-08-03 09:09:28 +0300

    Add layered deformation system base(not working yet)

---
+++ Source/ProjectShock/Enemies/PS_Ripper.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include <ProjectShock/Enemies/PS_Ripper.h>
+
+
+// Sets default values
+APS_Ripper::APS_Ripper()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APS_Ripper::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APS_Ripper::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
